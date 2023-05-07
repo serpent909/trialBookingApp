856d806f95b3a4a0b8339e7d6e0ef29f05a1d4b7
Matt <msmi200@aucklanduni.ac.nz>
2023-05-07 22:13:39 +1200

    schedules table populates automatically

--- modules/database.js
+++ modules/database.js
@@ -1,26 +1,131 @@
 const fs = require('fs');
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
 
 const DB_PATH = path.join(__dirname, '..', 'project-database.db');
 const SQL_SCRIPT_PATH = path.join(__dirname, '..', 'SQL', 'project-database-init-script.sql');
+const AVAILABILITY_DIR = path.join(__dirname, '..', 'config');
 
-// Create a new database connection
-const db = new sqlite3.Database(DB_PATH);
-
-// Read the SQL script file
 const sqlScript = fs.readFileSync(SQL_SCRIPT_PATH, 'utf8');
 
-// Execute the SQL script
-db.serialize(() => {
-  db.exec(sqlScript, (err) => {
-    if (err) {
-      console.error('Failed to execute the SQL script:', err);
-    } else {
-      console.log('SQL script executed successfully');
-    }
+function readAvailabilityJson(resource) {
+  const filePath = path.join(AVAILABILITY_DIR, `${resource}.json`);
+  const rawData = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(rawData);
+}
 
-    // Close the database connection
-    db.close();
+function generateScheduleEntries(availabilityJson, resourceId, resourceName) {
+
+  console.log(resourceName)
+
+  const availability = availabilityJson.availability;
+  const scheduleEntries = [];
+
+  availability.forEach((availabilityPeriod) => {
+    const startDate = new Date(availabilityPeriod.startDate);
+    const endDate = new Date(availabilityPeriod.endDate);
+
+    for (let currentDate = startDate; currentDate <= endDate; currentDate.setDate(currentDate.getDate() + 1)) {
+      const currentDayOfWeek = currentDate.toLocaleString('en-US', { weekday: 'long' });
+
+      const availabilityByDay = availabilityPeriod.availabilityByDay.find((item) => item.dayOfWeek === currentDayOfWeek);
+
+      if (availabilityByDay) {
+        const startTime = availabilityByDay.startTime;
+        const endTime = availabilityByDay.endTime;
+        const startDateTime = new Date(currentDate);
+        const endDateTime = new Date(currentDate);
+
+        startDateTime.setHours(parseInt(startTime.split(':')[0]), parseInt(startTime.split(':')[1]), 0, 0);
+        endDateTime.setHours(parseInt(endTime.split(':')[0]), parseInt(endTime.split(':')[1]), 0, 0);
+
+        scheduleEntries.push({
+          bookable_thing_id: resourceId,
+          name: resourceName,
+          start_time: toLocalISOString(startDateTime),
+          end_time: toLocalISOString(endDateTime),
+        });
+      }
+    }
   });
-});
+
+  return scheduleEntries;
+}
+
+function toLocalISOString(date) {
+  const tzOffset = date.getTimezoneOffset() * 60000;
+  const localISOTime = new Date(date - tzOffset).toISOString().slice(0, -1);
+  return localISOTime;
+}
+
+async function getResourceId(db, resourceName) {
+  const row = await db.get('SELECT id FROM bookable_things WHERE name = ?', [resourceName]);
+  return row ? row.id : null;
+}
+
+async function getResourceName(db, resourceName) {
+  const row = await db.get('SELECT name FROM bookable_things WHERE name = ?', [resourceName]);
+  return row ? row.name : null;
+}
+
+
+async function populateSchedules(db) {
+  const resources = [
+    { name: 'room1', type: 'Room' },
+    { name: 'room2', type: 'Room' },
+    { name: 'researcher', type: 'Researcher' },
+    { name: 'nurse', type: 'Nurse' },
+    { name: 'psychologist1', type: 'Psychologist' },
+    { name: 'psychologist2', type: 'Psychologist' },
+    { name: 'psychologist3', type: 'Psychologist' },
+    { name: 'psychologist4', type: 'Psychologist' },
+    { name: 'psychologist5', type: 'Psychologist' },
+    { name: 'psychologist6', type: 'Psychologist' },
+    { name: 'psychologist7', type: 'Psychologist' },
+    { name: 'psychologist8', type: 'Psychologist' }
+  ];
+
+  for (const resource of resources) {
+    const availabilityJson = readAvailabilityJson(resource.name);
+    const resourceId = await getResourceId(db, resource.name); 
+    const resourceName = await getResourceName(db, resource.name); 
+
+    const scheduleEntries = generateScheduleEntries(availabilityJson, resourceId, resourceName); 
+
+
+  // Clear availability table for the current resource
+  await db.run('DELETE FROM schedules WHERE name = ?', [resource.name]);
+
+
+    for (const entry of scheduleEntries) {
+
+      await db.run(
+        'INSERT INTO schedules (bookable_thing_id, name, start_time, end_time) VALUES (?, ?, ?, ?)',
+        [entry.bookable_thing_id, entry.name, entry.start_time, entry.end_time]
+      );
+    }
+  }
+}
+
+async function initDatabase() {
+  try {
+    const db = await sqlite.open({
+      filename: DB_PATH,
+      driver: sqlite3.Database,
+    });
+
+    await db.exec('PRAGMA foreign_keys = ON');
+    await db.exec(sqlScript);
+    console.log('SQL script executed successfully');
+
+    await populateSchedules(db);
+
+    return db;
+  } catch (err) {
+    console.error('Failed to execute the SQL script:', err);
+    throw err;
+  }
+}
+
+module.exports = initDatabase();
--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -1,8 +1,3 @@
-DROP TABLE IF EXISTS appointments;
-DROP TABLE IF EXISTS bookable_things;
-DROP TABLE IF EXISTS booked_times;
-DROP TABLE IF EXISTS schedules;
-
 CREATE TABLE IF NOT EXISTS
     appointments (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,7 +9,6 @@ CREATE TABLE IF NOT EXISTS
         appointment_type_id INTEGER NOT NULL,
         start_time TEXT NOT NULL,
         end_time TEXT NOT NULL,
-        FOREIGN KEY (participant_id) REFERENCES bookable_things (id),
         FOREIGN KEY (researcher_id) REFERENCES bookable_things (id),
         FOREIGN KEY (nurse_id) REFERENCES bookable_things (id),
         FOREIGN KEY (psychologist_id) REFERENCES bookable_things (id),
@@ -26,33 +20,59 @@ CREATE TABLE IF NOT EXISTS
 CREATE TABLE
     IF NOT EXISTS bookable_things (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
         type TEXT NOT NULL
     );
 
 -- Populate 'bookable_things' with predefined types if they don't exist
-INSERT OR IGNORE INTO bookable_things (type) SELECT 'Participant'    WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Participant');
-INSERT OR IGNORE INTO bookable_things (type) SELECT 'Researcher'     WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Researcher');
-INSERT OR IGNORE INTO bookable_things (type) SELECT 'Nurse'          WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Nurse');
+INSERT OR IGNORE INTO bookable_things (type, name) 
+SELECT 'Researcher', 'researcher'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Researcher');
 
--- Insert 8 rows with the 'Psychologist' type
-INSERT OR IGNORE INTO bookable_things (type)
-SELECT 'Psychologist'
-FROM (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8) seq
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM bookable_things
-    WHERE type = 'Psychologist'
-);
+INSERT OR IGNORE INTO bookable_things (type, name) 
+SELECT 'Nurse', 'nurse'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Nurse');
 
--- Insert 2 rows with the 'Room' type
-INSERT OR IGNORE INTO bookable_things (type)
-SELECT 'Room'
-FROM (SELECT 1 UNION SELECT 2) seq
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM bookable_things
-    WHERE type = 'Room'
-);
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Room', 'room1'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Room' AND name = 'room1');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Room', 'room2'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Room' AND name = 'room2');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Psychologist', 'psychologist1'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Psychologist' AND name = 'psychologist1');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Psychologist', 'psychologist2'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Psychologist' AND name = 'psychologist2');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Psychologist', 'psychologist3'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Psychologist' AND name = 'psychologist3');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Psychologist', 'psychologist4'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Psychologist' AND name = 'psychologist4');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Psychologist', 'psychologist5'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Psychologist' AND name = 'psychologist5');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Psychologist', 'psychologist6'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Psychologist' AND name = 'psychologist6');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Psychologist', 'psychologist7'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Psychologist' AND name = 'psychologist7');
+
+INSERT OR IGNORE INTO bookable_things (type, name)
+SELECT 'Psychologist', 'psychologist8'
+WHERE NOT EXISTS (SELECT 1 FROM bookable_things WHERE type = 'Psychologist' AND name = 'psychologist8');
+
 
 -- Create the 'booked_times' table
 CREATE TABLE IF NOT EXISTS booked_times (
@@ -65,12 +85,12 @@ CREATE TABLE IF NOT EXISTS booked_times (
     FOREIGN KEY (bookable_thing_id) REFERENCES bookable_things(id)
 );
 
+-- Create the 'schedules' table
 CREATE TABLE IF NOT EXISTS schedules (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    appointment_id INTEGER,
     bookable_thing_id INTEGER,
+    name TEXT,
     start_time TEXT NOT NULL,
     end_time TEXT NOT NULL,
-    FOREIGN KEY (appointment_id) REFERENCES appointments(id),
     FOREIGN KEY (bookable_thing_id) REFERENCES bookable_things(id)
 );
