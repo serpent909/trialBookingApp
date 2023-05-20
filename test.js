7dfb229aded256e887f4f6d8a4b030736a2a0fdc
Matt <msmi200@aucklanduni.ac.nz>
2023-05-20 14:08:13 +1200

    remove test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const moment = require('moment');
-
-
-let originalStartTime = '2023-05-01T15:30:00.000'
-let newStartTime = moment(originalStartTime).subtract(30, 'minutes').format('YYYY-MM-DDTHH:mm:ss');
-
-console.log(newStartTime)
